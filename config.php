2a368c702a31f657ccf53f882e7d2cb03ec33057
mirwansyahs <mirwansyahs1933@gmail.com>
2021-07-26 22:58:45 +0700

    first commit

---
+++ config.php
@@ -0,0 +1,40 @@
+<?php
+    session_start();
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+    
+    const base_url      = "http://localhost/bakso/";
+    const base_name     = "Bakso Yamin Kang Iim";
+    const favicon       = "AdminLTELogo.png";
+
+    const hostname   = "localhost";
+    const username   = "root";
+    const password   = "";
+    const database   = "spt_crm";
+
+    class Config{
+        public $con;
+
+        function __construct(){
+            $this->con = new mysqli(hostname, username, password, database);
+            if (!$this->con){
+                echo mysqli_error($this->con);
+            }
+        }
+
+        function swal($title = '', $text = '', $icon = ''){
+            $html = "<script>Swal.fire(
+                '".$title."',
+                '".$text."',
+                '".$icon."'
+              )</script>";
+            echo $html;
+        }
+
+        function redirect($url = ''){
+            echo "<meta http-equiv='refresh' content='0;".$url."'>";
+        }
+    }
+    
+?>
